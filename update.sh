da479e736bb04c522b600007bbc77fbcdd86e2aa
s890214 <sp8902142008@126.com>
2025-04-03 14:31:55 +0800

    Update update.sh
    Signed-off-by: s890214 <sp8902142008@126.com>

--- update.sh
+++ update.sh
@@ -154,7 +154,7 @@ install_small8() {
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf \
-        easytier luci-app-easytier
+        easytier luci-app-easytier luci-app-wrtbwmon luci-i18n-wrtbwmon-zh-cn
 }
 
 install_feeds() {
