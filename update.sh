0aa8740ea734d81fe48193da57d558873a66ea78
s890214 <sp8902142008@126.com>
2025-05-13 09:21:59 +0800

    Update update.sh
    Signed-off-by: s890214 <sp8902142008@126.com>

--- update.sh
+++ update.sh
@@ -823,7 +823,7 @@ main() {
     fix_compile_vlmcsd
     update_nss_diag
     update_menu_location
-    fix_compile_coremark
+    # fix_compile_coremark
     update_dnsmasq_conf
     add_backup_info_to_sysupgrade
     optimize_smartDNS
