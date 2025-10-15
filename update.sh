19ea4f14f5f5b4a6c66d116096579bbf57c2718c
s890214 <sp8902142008@126.com>
2025-10-15 13:55:26 +0800

    Update update.sh
    Signed-off-by: s890214 <sp8902142008@126.com>

--- update.sh
+++ update.sh
@@ -1064,7 +1064,7 @@ main() {
     update_golang
     change_dnsmasq2full
     fix_mk_def_depends
-    # add_wifi_default_set
+    add_wifi_default_set
     update_default_lan_addr
     remove_something_nss_kmod
     update_affinity_script
