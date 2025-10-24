8a3636a2afc81713efaa0593b7bba0e6e12ade16
s890214 <sp8902142008@126.com>
2025-10-24 13:32:03 +0800

    Update update.sh
    Signed-off-by: s890214 <sp8902142008@126.com>

--- update.sh
+++ update.sh
@@ -334,7 +334,7 @@ apply_hash_fixes() {
         "smartdns"
 }
 
-() {
+update_ath11k_fw() {
     local makefile="$BUILD_DIR/package/firmware/ath11k-firmware/Makefile"
     local new_mk="$BASE_PATH/patches/ath11k_fw.mk"
     local url="https://raw.githubusercontent.com/VIKINGYFY/immortalwrt/refs/heads/main/package/firmware/ath11k-firmware/Makefile"
