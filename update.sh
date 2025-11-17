8aa092e6a3a59000ebee2872efb50e07ac8663fd
s890214 <sp8902142008@126.com>
2025-11-17 17:20:13 +0800

    Update update.sh
    Signed-off-by: s890214 <sp8902142008@126.com>

--- update.sh
+++ update.sh
@@ -257,6 +257,9 @@ fix_default_set() {
             \cp -f "$BASE_PATH/patches/tempinfo" "$BUILD_DIR/package/emortal/autocore/files/tempinfo"
         fi
     fi
+
+	# 删除luci-app-attendedsysupgrade在概览页面的升级提示
+	rm -rf "$BUILD_DIR/feeds/luci/applications/luci-app-attendedsysupgrade/htdocs/luci-static/resources/view/status/include/11_upgrades.js"
 }
 
 fix_miniupnpd() {
